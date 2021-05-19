2d4b8c85919f38c46441f9ccbd8fc273e6d06baa
Jen <6406037+usrrname@users.noreply.github.com>
2021-05-19 15:04:47 -0400

    Upgrade jest-presets-angular

--- setup-jest.ts
+++ setup-jest.ts
@@ -1,5 +1,6 @@
 // src/jest-config/setup.ts
-import 'jest-preset-angular';
-import './globalMocks';
+import 'jest-preset-angular/setup-jest';
+
+import './src/jest-config/globalMocks';
 
 Object.defineProperty(global, 'Promise', { writable: false, value: global.Promise });
